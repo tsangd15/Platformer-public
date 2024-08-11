019287807b3c26778c695c8c117df2b4a9510f3d
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:30 +0100

    New Aiming Implementation Reduced fire cooldown from 500 to 320 Added parameter projectile_velocity to fire method Replaced aim implementation for fire method Used centre coordinates for projectile start position for uniformity

--- _player.py
+++ _player.py
@@ -11,20 +11,17 @@ class Player(Entity):
         super().__init__(color, width, height, startx, starty)
         self.projectiles = pygame.sprite.Group()
         self.lastfired = pygame.time.get_ticks()
-        self.firecooldown = 500
+        self.firecooldown = 320
 
-    def fire(self):
+    def fire(self, projectile_velocity):
         """Spawns a projectile and adds it to the projectiles sprite group
         attribute."""
         now = pygame.time.get_ticks()
         if now - self.lastfired >= self.firecooldown:
             self.lastfired = now
 
-            if self.movingleft:
-                projectile_velocity_x = -10
-            else:
-                projectile_velocity_x = 10
+            projectile = Projectile(BLUE, self.rect.centerx,
+                                    self.rect.centery, projectile_velocity[0],
+                                    projectile_velocity[1])
 
-            projectile = Projectile(BLUE, self.rect.x, self.rect.y,
-                                    projectile_velocity_x, 0)
             self.projectiles.add(projectile)
