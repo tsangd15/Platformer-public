0931a4aa6c85ae1cb4366d2a0c2fc35591503b82
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:50 +0100

    Add Menu to Level Fail Screen
    This commit adds a menu to allow navigation back to the root menu, save
    score screen or quit.

--- _level_fail.py
+++ _level_fail.py
@@ -1,8 +1,10 @@
 """Game Level - Fail Module"""
 import pygame
 from _screen import Screen
-from _settings import WINDOW_WIDTH, WINDOW_HEIGHT, BLUE
+from _settings import (WINDOW_WIDTH, BLUE, BLACK, RED, CYAN, YELLOW)
 from _text import Text
+from _button import Button
+from _functions import is_point_within_rect, return_button
 
 
 class LevelFail(Screen):
@@ -11,17 +13,43 @@ class LevelFail(Screen):
         super().__init__(screens)
 
         # add screen specific event handlers to list of event handlers
-        self.event_handlers.extend((self.handle_events_keyboard,))
+        self.event_handlers.extend((self.handle_events_keyboard,
+                                   self.handle_events_mouse))
 
         # add the text sprites
         self.add_text()
+        self.add_buttons()
 
     def add_text(self):
         """Add text to sprite group to be blitted to screen."""
-        text_main = Text("Level Failed!", (WINDOW_WIDTH-60,
-                         WINDOW_HEIGHT-60), "middle_center", BLUE, None,
-                         WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+        text_main = Text("Level Failed!", 60, "top_center", BLUE, None,
+                         WINDOW_WIDTH/2, 85)
         self.sprites.add(text_main)
+
+    def add_buttons(self):
+        """Instantiate and add each button to sprites and buttons
+        sprite group.
+        A for loop iterates through all the menu items listed in self.items
+        and creates a Button instance for each which is then added to the
+        sprites and buttons sprite groups."""
+        # define button colours
+        button_idlecolor = (BLACK, RED)
+        button_hovercolor = (CYAN, RED)
+        button_clickcolor = (CYAN, YELLOW)
+
+        # each iteration height increments 55
+        # zip function to handle parallel iterator variables: item_name, height
+        for item_name, height in zip(self.screens,
+                                     range(190, 190+55*len(self.screens)+1, 55)
+                                     ):
+            button = Button(350, 50, item_name, 30, "top_center",
+                            button_idlecolor, button_hovercolor,
+                            button_clickcolor, WINDOW_WIDTH/2, height)
+            self.sprites.add(button)
+            self.buttons.add(button)
+
+        # set top button as highlighted
+        self.set_selected_hover()
 
     def handle_events_keyboard(self, event):
         """Handle keyboard related events. If the given event matches, the
@@ -30,17 +58,46 @@ class LevelFail(Screen):
         if event.type == pygame.QUIT:
             self.terminate()
 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:  # up arrow
+                self.select_up = True
+            elif event.key == pygame.K_DOWN:  # down arrow
+                self.select_down = True
+            elif (event.key == pygame.K_SPACE or  # spacebar
+                  event.key == pygame.K_KP_ENTER or  # keypad enter
+                  event.key == pygame.K_RETURN):  # main enter key
+                self.confirmed = True
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                self.select_up = False
+            elif event.key == pygame.K_DOWN:
+                self.select_down = False
+
         # return to calling line if the event matched
         else:
             return False  # no match
         return True  # match
 
-    def update(self):
-        """Update the menu by checking for any events and updating attributes
-        and button states as needed."""
+    def handle_events_mouse(self, event):
+        """Handle mouse related events. If the given event matches, the
+        corresponding actions for that matched event are carried out."""
+        if event.type == pygame.MOUSEMOTION:
+            self.cursor_moved = True
 
-        # removed update_cursor and update_selected calls as no menu for
-        # screen yet
-        self.handle_events()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:  # left click
+                # get the button instance that is selected
+                selected_button = return_button(self.selected,
+                                                self.buttons)
 
-        return self.process_next_screen()
+                # if button returned, check cursor is on the button
+                if selected_button is not None:
+                    if is_point_within_rect(event.pos, selected_button):
+                        # let menu know to call associated method
+                        self.confirmed = True
+
+        # return to calling line if the event matched
+        else:
+            return False  # no match
+        return True  # match
