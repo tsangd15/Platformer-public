779480858648f1c9edf90a654ef0c673ea7a96ee
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:28 +0100

    Added method, fire Added fire method to spawn projectile at player's location Added docstring and required imports for projectile implementation

--- _player.py
+++ _player.py
@@ -1,6 +1,15 @@
+"""Player Class Module"""
+import pygame
 from _entity import Entity
+from _projectile import Projectile
+from _settings import BLUE
 
 
 class Player(Entity):
     def __init__(self, color, width, height, startx=430, starty=400):
         super().__init__(color, width, height, startx, starty)
+        self.projectileslist = pygame.sprite.Group
+
+    def fire(self):
+        projectile = Projectile(BLUE, self.rect.x, self.rect.y, 10, 0)
+        return projectile
