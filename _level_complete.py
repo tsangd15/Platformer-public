07669b69a2e6e02b6ecb55b70b14139aaeb404ea
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:47 +0100

    Inherit Screen into LevelComplete Class

--- _level_complete.py
+++ _level_complete.py
@@ -1,40 +1,46 @@
 """Game Level - Complete Module"""
 import pygame
+from _screen import Screen
 from _settings import WINDOW_WIDTH, WINDOW_HEIGHT, BLUE
 from _text import Text
 
 
-class LevelComplete():
+class LevelComplete(Screen):
     """Class for level complete screen"""
     def __init__(self, screens):
-        self.sprites = pygame.sprite.Group()
-        self.screens = screens
-        self.next_screen_index = -1
+        super().__init__(screens)
 
+        # add screen specific event handlers to list of event handlers
+        self.event_handlers.extend((self.handle_events_keyboard,))
+
+        # add the text sprites
         self.add_text()
 
     def add_text(self):
-        """Add text to sprite group to be blitted to screen."""
+        """Add text instances to sprite group to be blitted to screen."""
         text_main = Text("Level Completed!", (WINDOW_WIDTH-60,
                          WINDOW_HEIGHT-60), "middle_center", BLUE, None,
                          WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
         self.sprites.add(text_main)
 
-    def handle_events(self):
-        """Get and handle events in the pygame event queue."""
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.next_screen_index = 0
+    def handle_events_keyboard(self, event):
+        """Handle keyboard related events. If the given event matches, the
+        corresponding actions for that matched event are carried out."""
+        # altf4 or window close button invokes pygame.QUIT
+        if event.type == pygame.QUIT:
+            self.terminate()
 
-    def next_screen(self):
-        """Return the next screen to display (returning None will mean the
-        current screen will continue to be displayed."""
-        if self.next_screen_index != -1:
-            return self.screens[self.next_screen_index]
-        return None
+        # return to calling line if the event matched
+        else:
+            return False  # no match
+        return True  # match
 
     def update(self):
-        """Handle events and return next screen to display."""
+        """Update the menu by checking for any events and updating attributes
+        and button states as needed."""
+
+        # removed update_cursor and update_selected calls as no menu for
+        # screen yet
         self.handle_events()
 
-        return self.next_screen()
+        return self.process_next_screen()
