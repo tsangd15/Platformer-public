b767273d0c6908e7f473a74ff23ac1e0512d084c
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:32 +0100

    Implement progress bars
    Health and stamina progress bars created to store and display the
    metrics. Additional imports were used to acheive this. Stamina is yet to
    be implemented fully, only the bar is visible.

--- _player.py
+++ _player.py
@@ -2,7 +2,8 @@
 import pygame
 from _entity import Entity
 from _projectile import Projectile
-from _settings import PURPLE
+from _settings import WINDOW_WIDTH, GREEN, RED, YELLOW, PURPLE
+from _progressbar import ProgressBar
 
 
 class Player(Entity):
@@ -11,12 +12,17 @@ class Player(Entity):
         super().__init__(color, width, height, startx, starty)
         self.projectiles = pygame.sprite.Group()
         self.defaulthealth = 25
-        self.health = self.defaulthealth
         self.lives = 3
         self.gameover = False
         self.lastfired = pygame.time.get_ticks()
         self.firecooldown = 320
         self.number = 0
+        self.stats = pygame.sprite.Group()
+        self.health = ProgressBar(300, 20, RED, YELLOW, WINDOW_WIDTH/2-150,
+                                  20, self.defaulthealth)
+        self.stamina = ProgressBar(300, 20, GREEN, YELLOW, WINDOW_WIDTH/2-150,
+                                   40, 100)
+        self.stats.add(self.health.bars, self.stamina.bars)
 
     def fire(self, projectile_velocity):
         """Spawns a projectile and adds it to the projectiles sprite group
@@ -33,14 +39,14 @@ class Player(Entity):
 
     def hit(self):
         """Method to reduce health when hit by projectile."""
-        self.health -= 5
+        self.health.value -= 5
         print("hit", self.number)
         self.number += 1
 
     def respawn(self):
         """Decrease lives and reset health for respawn."""
         self.lives -= 1
-        self.health = self.defaulthealth
+        self.health.value = self.defaulthealth
         self.rect.x, self.rect.y = self.startx, self.starty
 
     def update(self):
@@ -50,6 +56,6 @@ class Player(Entity):
             self.gameover = True
             print("GAMEOVER")
         else:
-            if self.health <= 0:
+            if self.health.value <= 0:
                 self.respawn()
                 print("RESPAWNED")
