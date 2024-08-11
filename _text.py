371b76f9f788bf908b4cdc7cbfbbaadd81e70e43
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:33 +0100

    Add update method, Add property decorators
    Created update method to update image and rect, this will remove code
    repetition in the __init__ method.
    
    Used property decorators for text, startx and starty to introduce input
    validation and sanitation as they are values subject to change.

--- _text.py
+++ _text.py
@@ -9,24 +9,22 @@ class Text(pygame.sprite.Sprite):
     def __init__(self, text, size, fgcolour, bgcolour, startx, starty):
         super().__init__()
 
-        self.text = text
+        # store arguments
+        self._text = text
         self.fgcolour = fgcolour
         self.bgcolour = bgcolour
+        self._startx = startx
+        self._starty = starty
+
         self.size = size
         self.number = 0
 
-        # font file/name, font size
+        # instantiate font object instance
+        # parameters: font file/name, font size
         self.font = pygame.freetype.Font("PressStart2P-Regular.ttf", self.size)
 
-        # function returns text as rendered syrface and rect object instance
-        # parameters: text, fgcolour, bgcolour
-        self.image, self.rect = self.font.render(self.text, self.fgcolour,
-                                                 self.bgcolour)
-
-        # set location of text
-        self.startx = startx
-        self.starty = starty
-        self.rect.x, self.rect.y = startx, starty
+        # set the sprite's image and rect
+        self.update()
 
     def rect_info(self):
         """For debugging, outputs generated text sprite's rect details
@@ -39,6 +37,47 @@ class Text(pygame.sprite.Sprite):
         """For debugging, increments and updates the text sprite"""
         self.number += 1
         self.text = "Score: " + str(self.number)
+
+    def update(self):
+        """Update the sprite's image and rect"""
+        # function returns text as rendered syrface and rect object instance
+        # parameters: text, fgcolour, bgcolour
         self.image, self.rect = self.font.render(self.text, self.fgcolour,
                                                  self.bgcolour)
         self.rect.x, self.rect.y = self.startx, self.starty
+
+    @property
+    def text(self):
+        """Property decorator for text attribute"""
+        return self._text
+
+    @text.setter
+    def text(self, new_text):
+        if isinstance(new_text, str):
+            self._text = new_text
+        else:
+            raise Exception("new_text not string type")
+
+    @property
+    def startx(self):
+        """Property decorator for startx attribute"""
+        return self._startx
+
+    @startx.setter
+    def startx(self, new_startx):
+        if isinstance(int(new_startx), int):
+            self._startx = int(new_startx)
+        else:
+            raise Exception("new_startx not int type")
+
+    @property
+    def starty(self):
+        """Property decorator for starty attribute"""
+        return self._starty
+
+    @starty.setter
+    def starty(self, new_starty):
+        if isinstance(int(new_starty), int):
+            self._starty = int(new_starty)
+        else:
+            raise Exception("new_starty not int type")
