b49e9cde53c99126d89fa092fd8ba7219aaf593a
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:32 +0100

    Add Progress Bar Module
    Progress bars will be directly used when creating progress bars on the
    screen. Progress bars are made of 2 instantiated Bar objects. 1 for the
    background and 1 for the foreground. Only the foreground bar will change
    size to display metrics.

---
+++ _progressbar.py
@@ -0,0 +1,43 @@
+"""Progress Bar Module"""
+import pygame
+from _bar import Bar
+
+
+class ProgressBar():
+    """Class for progress bars"""
+    def __init__(self, width, height, fgcolour, bgcolour, startx, starty,
+                 maximum):
+        # create individual bars and add to sprite group
+        self.outline = 2
+        self.bars = pygame.sprite.Group()
+        self.bar_bg = Bar(bgcolour, width, height, startx, starty)
+        self.bar_fg = Bar(fgcolour, width-(2*self.outline),
+                          height-(2*self.outline), startx+(self.outline),
+                          starty+(self.outline))
+        self.bars.add(self.bar_bg, self.bar_fg)
+
+        self.maximum = maximum
+        self._value = maximum
+
+    @property
+    def value(self):
+        """Property decorator for value attribute"""
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        """Property decorator setter for value attribute"""
+        if 0 <= new_value <= self.maximum:
+            self._value = new_value
+            self.update()
+        else:
+            raise Exception("Invalid progress bar 'value'")
+
+    def update(self):
+        """Update progress bar with new capacity
+
+        This method sets a new capacity for the bar as a percentage (out of
+        the maximum, self.maximum). The bar's update() method is then called to
+        create the new image and rect for the new capacity."""
+        self.bar_fg.capacity = (self.value/self.maximum)*100
+        self.bar_fg.update()
