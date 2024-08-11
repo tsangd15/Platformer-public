d6ceb9fcbf316ceee5f2200cc0f04587e6a83201
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:45 +0100

    Use String for next_screen Referencing
    Instead of referencing the next screen to display using an index of
    the screens attribute using a next_screen_index attribute, the next
    screen is explicitly specified using the next_screen attribute.
    
    The previous next_screen method has been renamed to process_next_screen.
    
    Indexes are no longer used as you cannot immediately determine what each
    index represents and adding new screens in between will cause the
    indexes to change for other screens.
    
    The next screen is stored in the next_screen attribute explicitly, e.g.
    "resume", or None if there's no next screen yet.
    
    The next_screen attribute has a property decorator applied to check if a
    valid screen is set to next_screen.
    
    A tuple of screens available is passed to the class instead of being
    defined in the constructor directly.
    
    This change is due to happen to all applicable classes soon.

--- _level_pause.py
+++ _level_pause.py
@@ -6,9 +6,9 @@ from _text import Text
 
 class LevelPause():
     """Class for level pause screen"""
-    def __init__(self):
-        self.screens = ("resume", "quit")
-        self.next_screen_index = -1
+    def __init__(self, screens):
+        self.screens = screens
+        self._next_screen = None
 
         self.sprites = pygame.sprite.Group()
 
@@ -35,27 +35,39 @@ class LevelPause():
         """Get and handle events in the pygame event queue."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.next_screen_index = 1  # self.screens[1] = "quit"
+                self.next_screen = "quit"
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    self.next_screen_index = 0  # self.screens[0] = "resume"
+                    self.next_screen = "resume"
 
-    def next_screen(self):
+    def process_next_screen(self):
         """Return the next screen to display (returning None will mean the
         current screen will continue to be displayed."""
-        if self.next_screen_index != -1:
-            # if "resume" is selected:
-            if self.next_screen_index == 0:
-                # calculate the total time paused and save
-                self.pause_end = pygame.time.get_ticks()
-                self.duration = self.pause_end - self.pause_begin
-            # return next screen's name
-            return self.screens[self.next_screen_index]
-        return None
+        # if "resume" is selected:
+        if self.next_screen == "resume":
+            # additional steps if resuming
+            # calculate the total time paused and save
+            self.pause_end = pygame.time.get_ticks()
+            self.duration = self.pause_end - self.pause_begin
+        # return next screen's name
+        return self.next_screen
 
     def update(self):
         """Handle events and return next screen to display."""
         self.handle_events()
 
-        return self.next_screen()
+        return self.process_next_screen()
+
+    @property
+    def next_screen(self):
+        """Property decorator getter for next_screen attribute"""
+        return self._next_screen
+
+    @next_screen.setter
+    def next_screen(self, screen_name):
+        """Property decorator setter for next_screen attribute"""
+        if screen_name in self.screens:
+            self._next_screen = screen_name
+        else:
+            raise Exception(f"Invalid screen name passed: {screen_name}")
