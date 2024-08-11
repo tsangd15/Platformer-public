b5e68f8b8b6e104cec384372100d16451ffb86ff
tsangd15 <77072827+tsangd15@users.noreply.github.com>
2024-08-11 16:52:29 +0100

    Implemented projectile firing cooldown

--- _player.py
+++ _player.py
@@ -10,14 +10,21 @@ class Player(Entity):
     def __init__(self, color, width, height, startx=430, starty=400):
         super().__init__(color, width, height, startx, starty)
         self.projectiles = pygame.sprite.Group()
+        self.lastfired = pygame.time.get_ticks()
+        self.firecooldown = 500
 
     def fire(self):
         """Spawns a projectile and adds it to the projectiles sprite group
         attribute."""
-        if self.movingleft:
-            projectile_velocity_x = -10
-        else:
-            projectile_velocity_x = 10
-        projectile = Projectile(BLUE, self.rect.x, self.rect.y,
-                                projectile_velocity_x, 0)
-        self.projectiles.add(projectile)
+        now = pygame.time.get_ticks()
+        if now - self.lastfired >= self.firecooldown:
+            self.lastfired = now
+
+            if self.movingleft:
+                projectile_velocity_x = -10
+            else:
+                projectile_velocity_x = 10
+
+            projectile = Projectile(BLUE, self.rect.x, self.rect.y,
+                                    projectile_velocity_x, 0)
+            self.projectiles.add(projectile)
